1c2a1881ec5532f002af07491a900f3b1cd45a8c
Mladen Srdić <m.s.qubits@gmail.com>
2020-06-04 00:28:23 +0200

    Added shortcut keys: Cmd+Shift+L for marking a line and Cmd+Shift+K for removing marked lines.

--- reducto.js
+++ reducto.js
@@ -3,3 +3,15 @@ chrome.browserAction.onClicked.addListener(function(tab) {
         file: 'reducto_content.js'
       });
 });
+
+chrome.commands.onCommand.addListener(function(command) {
+    if(command == "reducto-selection") {
+        chrome.tabs.executeScript({
+            file: 'reducto_content.js'
+        });
+    } else if(command == "reducto-remove") {
+        chrome.tabs.executeScript({
+            file: 'reducto_remove.js'
+        });
+    }
+  });
